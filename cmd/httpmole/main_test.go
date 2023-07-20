dbf5c7b2f3195203826fb4b8e6fd60bfc6c4ba83
José Carlos Chávez <jcchavezs@gmail.com>
2023-07-20 07:12:33 +0200

    fix: header values with semicolon.

--- cmd/httpmole/main_test.go
+++ cmd/httpmole/main_test.go
@@ -7,6 +7,7 @@ import (
 )
 
 func TestToHeadersMap(t *testing.T) {
-	h := toHeadersMap([]string{"a:b"})
-	require.Equal(t, "b", h.Get("a"))
+	h := toHeadersMap([]string{"x-request-id:abc123", "location:http://localhost:8080/login"})
+	require.Equal(t, "abc123", h.Get("x-request-id"))
+	require.Equal(t, "http://localhost:8080/login", h.Get("location"))
 }
